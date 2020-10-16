871603018b8319e9aacbc42108393f91133d284c
mwafy <mwafy@pop-os.localdomain>
2020-10-16 18:11:05 +0200

    icons added

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,11 +1,37 @@
 import React from 'react';
 import Nav from './Nav';
 import Header from './Header';
+import '../styles/App.scss';
+import Airplane from './assets/airplane-travel.svg';
+import Balloon from './assets/hot-air-balloon.svg';
+import Tropical from './assets/tropical.svg';
+import Cruise from './assets/cruise.svg';
+import Mountain from './assets/mountain.svg';
 const App = () => {
     return (
-        <div>
+        <div className='app'>
             <Nav />
             <Header />
+            <div className='container'>
+                <div className='travel-icons'>
+                    <div className='icon'>
+                        <img src={Airplane} alt='' />
+                        <p>travel anywhere</p>
+                    </div>
+                    <div className='icon'>
+                        <img src={Cruise} alt='' />
+                        <p>Cruise</p>
+                    </div>
+                    <div className='icon'>
+                        <img src={Balloon} alt='' />
+                        <p>air balloon</p>
+                    </div>
+                    <div className='icon'>
+                        <img src={Tropical} alt='' />
+                        <p>enjoy summer</p>
+                    </div>
+                </div>
+            </div>
         </div>
     );
 };
--- src/index.js
+++ src/index.js
@@ -2,5 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './styles/index.scss';
 import App from './components/App.jsx';
+import '../node_modules/@fortawesome/fontawesome-free/css/all.css';
 // inject javascript
 ReactDOM.render(<App />, document.getElementById('root'));
