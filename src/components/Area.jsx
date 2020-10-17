be66a442a4497a8f8d3067b44c9677acfacf5a4f
mwafy092 <mwafy092@gmail.com>
2020-10-17 13:54:55 +0200

    area addded

---
+++ src/components/Area.jsx
@@ -0,0 +1,30 @@
+import React, { useEffect, useState } from 'react';
+import '../styles/City.scss';
+const Area = (props) => {
+    const [areas, setAreas] = useState([]);
+    const getArea = async () => {
+        const URL = `http://46.101.108.59/api/country/${props.countryId}/city/${props.cityId}/area`;
+        const PROXY = `https://cors-anywhere.herokuapp.com/`;
+
+        const response = await fetch(PROXY + URL)
+            .then((response) => response.json())
+            .then((data) => setAreas(data.data));
+    };
+    useEffect(() => {
+        getArea();
+    }, [props.cityId]);
+    return (
+        <div className='area'>
+            <label htmlFor='area'>Area</label>
+            <select name='area' id='area'>
+                {areas.map((area) => (
+                    <option value={area.attributes.name} key={area.id}>
+                        {area.attributes.name}
+                    </option>
+                ))}
+            </select>
+        </div>
+    );
+};
+
+export default Area;
--- src/components/City.jsx
+++ src/components/City.jsx
@@ -1,7 +1,10 @@
 import React, { useEffect, useState } from 'react';
 import '../styles/City.scss';
+import Area from './Area';
 const City = (props) => {
     const [cities, setCities] = useState([]);
+    const [cityId, setCityId] = useState();
+
     const getCity = async () => {
         const URL = `http://46.101.108.59/api/country/${props.id}/city`;
         const PROXY = `https://cors-anywhere.herokuapp.com/`;
@@ -13,16 +16,24 @@ const City = (props) => {
     useEffect(() => {
         getCity();
     }, [props.id]);
+    const setSelect = (id) => {
+        setCityId(id);
+    };
     return (
         <div className='city'>
             <label htmlFor='city'>city</label>
-            <select name='city' id='city'>
+            <select
+                name='city'
+                id='city'
+                onChange={(e) => setSelect(e.target.value)}
+            >
                 {cities.map((city) => (
-                    <option value={city.attributes.name} key={city.id}>
+                    <option value={city.id} key={city.id}>
                         {city.attributes.name}
                     </option>
                 ))}
             </select>
+            <Area cityId={cityId} countryId={props.id} />
         </div>
     );
 };
